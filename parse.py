9738cf8442345dad10f6175f0b69d338e9388d6f
gndowns <gabriel.downs@shopify.com>
2017-06-29 18:10:06 -0400

    iterate over all threads

--- parse.py
+++ parse.py
@@ -4,4 +4,20 @@ from threads.threads import Threads, Thread, Message
 def parse(messages_path):
   threads = Threads()
 
+  with open(messages_path) as fp:
+    messages_html = fp.read()
+
+  h1_tag_index = messages_html.find("<h1>", 0) + len("<h1>")
+  # start increments forward as threads are read
+  start = messages_html.find("</h1>", h1_tag_index)
+
+  threads.owner = messages_html[h1_tag_index:start].strip()
+
+  tag = '<div class="thread">'
+  start = messages_html.find(tag, 0)
+  while(start != -1):
+    start += len(tag)
+
+    start = messages_html.find(tag, start)
+
 parse(sys.argv[1])
